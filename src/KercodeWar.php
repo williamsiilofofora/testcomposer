77ff50e6c3e2cc8bfbf6785f793d3a6d09d0c95e
Williams KUBIAK <wkubiak@561557n.ac-rennes.fr>
2019-02-01 16:04:30 +0100

    ajout yaml

--- src/KercodeWar.php
+++ src/KercodeWar.php
@@ -1,31 +1,95 @@
 <?php
+
+use Symfony\Component\Yaml\Yaml;
+
+
 class KercodeWar
 {
-    public static function weekday($number)
+    public static function weekday($number, $lang = "en")
     {
-        if ($number === 1) {
-            return 'Sunday';
+        if ($lang === "fr"){
+            $content = Yaml::parse(file_get_contents("fr.yml"));
+        }else{
+            $content = Yaml::parse(file_get_contents("en.yml"));
         }
-        if ($number === 2) {
-            return 'Monday';
+        if ($number >= 1 && $number <= 7) {
+            return $content['weekdays']['days'][$number - 1];
+          } else {
+            return $content['weekdays']['error'];
+          }
         }
-        if ($number === 3) {
-            return 'Tuesday';
-        }
-        if ($number === 4) {
-            return 'Wednesday';
-        }
-        if ($number === 5) {
-            return 'Thursday';
-        }
-        if ($number === 6) {
-            return 'Fryday';
-        }
-        if ($number === 7) {
-            return 'Saturday';
+
+       /* {
+            eval(\Psy\sh());
+        }*/
+       /* if ($lang === "fr") {
+            if ($number === 1) {
+                return 'Dimanche';
+            }
+            if ($number === 2) {
+                return 'Lundi';
+            }
+            if ($number === 3) {
+                return 'Mardi';
+            }
+            if ($number === 4) {
+                return 'Mercredi';
+            }
+            if ($number === 5) {
+                return 'Jeudi';
+            }
+            if ($number === 6) {
+                return 'Vendredi';
+            }
+            if ($number === 7) {
+                return 'Samedi';
+            } else {
+                return ' Erreur, entrez un numeros entre 1 et 7 s\'il vous plais.';
+                KercodeWar::weekday('plop');
+            }
+        } else {
+            if ($number === 1) {
+                return 'Sunday';
+            }
+            if ($number === 2) {
+                return 'Monday';
+            }
+            if ($number === 3) {
+                return 'Tuesday';
+            }
+            if ($number === 4) {
+                return 'Wednesday';
+            }
+            if ($number === 5) {
+                return 'Thursday';
+            }
+            if ($number === 6) {
+                return 'Friday';
+            }
+            if ($number === 7) {
+                return 'Saturday';
+            } else {
+                return 'Wrong, please enter a number beetwen 1 and 7';
+                KercodeWar::weekday('plop');
+            }
+        }*/
+
+
+
+
+
+
+    public static function isweekend($number)
+    {
+        if ($number === 1 || $number === 7) {
+            return true;
+        } elseif ($number === 2 || $number === 3 || $number === 4 || $number === 5 || $number === 6) {
+
+            return false;
         } else {
             return 'Wrong, please enter a number beetwen 1 and 7';
-            KercodeWar::weekday('plop');
+            KercodeWar::isweeken('plop');
         }
     }
+
 }
--- test/KercodeWarTest.php
+++ test/KercodeWarTest.php
@@ -6,15 +6,47 @@ use PHPUnit\Framework\TestCase;
 
 class KercodeWarTest extends TestCase
 {
+    const WEEKDAYS = [
+        1 => 'Sunday',
+        2 => 'Monday',
+        3 => 'Tuesday',
+        4 => 'Wednesday',
+        5 => 'Thursday',
+        6 => 'Friday',
+        7 => 'Saturday',
+
+    ];
+    const FRENCH_WEEKDAYS = [
+        1 => 'Dimanche',
+        2 => 'Lundi',
+        3 => 'Mardi',
+        4 => 'Mercredi',
+        5 => 'Jeudi',
+        6 => 'Vendredi',
+        7 => 'Samedi',
+
+    ];
+    public function testFrenchWithValidWeekdayValue()
+    {
+        foreach (self::FRENCH_WEEKDAYS as $key => $value) {
+            $this->assertEquals(
+                $value,
+                KercodeWar::weekday($key, 'fr')
+            );
+        }
+    }
+    public function testFrenchWithInvalidWeekdayValue()
+    {
+        $this->assertEquals(
+            'Erreur, entrez un numeros entre 1 et 7 s\'il vous plais.',
+            KercodeWar::weekday('plop', 'fr')
+        );
+    }
     public function testWeekDays()
     {
-        $this->assertEquals('Sunday', KercodeWar::weekday(1));
-        $this->assertEquals('Monday', KercodeWar::weekday(2));
-        $this->assertEquals('Tuesday', KercodeWar::weekday(3));
-        $this->assertEquals('Wednesday', KercodeWar::weekday(4));
-        $this->assertEquals('Thursday', KercodeWar::weekday(5));
-        $this->assertEquals('Fryday', KercodeWar::weekday(6));
-        $this->assertEquals('Saturday', KercodeWar::weekday(7));
+        foreach (self::WEEKDAYS as $key => $value) {
+            $this->assertEquals($value, KercodeWar::weekday($key));
+        }
     }
     public function testWithInvalidParam()
     {
@@ -24,5 +56,12 @@ class KercodeWarTest extends TestCase
         );
 
     }
-
-} 
+    public function testNumberInWeekend()
+    {
+        foreach (self::WEEKDAYS as $key => $value) {
+            $res = ($value === 'Sunday' || $value === 'Saturday');
+            $this->assertEquals($res, KercodeWar::isweekend($key));
+        }
+    }
+}
+        
